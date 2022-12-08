fd12b04e4b8ece912bc4352c437e56435995a0e3
CaptureB <airdavid62@gmail.com>
2022-12-08 14:01:56 -0700

    Added functions to javascript,displaying questions

--- quiz.js
+++ quiz.js
@@ -51,3 +51,90 @@ let questions = [
         answer: 2,
     },
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 5
+
+startQuiz = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        
+        return window.location.assign('')
+    }
+
+    questionCounter++
+    progressText.innerHTML = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    trueAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!trueAnswers) return
+
+        trueAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct':
+        'wrong'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 60)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText =score
+}
+
+let time = 1;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown(){
+    let quizTimer = setInterval(function(){
+    if(quizTime <= 0) {
+        clearInterval(quizTimer);
+        showScores();
+    } else {
+        quizTime--;
+        let sec = Math.floor(quizTime % 60);
+        let min = Math.floor(quizTime / 60) % 60;
+        counting.innerHTML = `TIME: ${min} : ${sec}`;   
+    }
+},1000);
+}
+
+startCountdown()
+
+startQuiz()
